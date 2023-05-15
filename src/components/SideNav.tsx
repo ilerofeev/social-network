2e1940835d6a7f08c8cbabfa951d6a5342a9bbe2
Ilya Erofeev <dev.erofeev@gmail.com>
2023-05-15 21:38:25 +0600

    _app, SideNav: create base structure

---
+++ src/components/SideNav.tsx
@@ -0,0 +1,31 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
+import Link from 'next/link';
+
+export function SideNav() {
+  const session = useSession();
+  const user = session.data?.user;
+
+  return (
+    <nav className="sticky top-0 px-2 py-4">
+      <ul className="flex flex-col items-start gap-2 whitespace-nowrap">
+        <li>
+          <Link href="/home">home</Link>
+        </li>
+        {user && (
+          <li>
+            <Link href={`/profiles/${user.id}`}>Profiles</Link>
+          </li>
+        )}
+        {user ? (
+          <button type="button" onClick={() => void signOut()}>
+            Log Out
+          </button>
+        ) : (
+          <button type="button" onClick={() => void signIn()}>
+            Log In
+          </button>
+        )}
+      </ul>
+    </nav>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,30 @@
 import { type AppType } from 'next/app';
 import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
-
+import Head from 'next/head';
 import { api } from '~/utils/api';
-
 import '~/styles/globals.css';
+import { SideNav } from '~/components/SideNav';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => (
   <SessionProvider session={session}>
-    <Component {...pageProps} />
+    <Head>
+      <title>Social Network</title>
+      <meta
+        name="description"
+        content="This is a full-stack social network project"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div className="container mx-auto flex items-start">
+      <SideNav />
+      <div className="min-h-screen flex-grow border-x">
+        <Component {...pageProps} />
+      </div>
+    </div>
   </SessionProvider>
 );
 
