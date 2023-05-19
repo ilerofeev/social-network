dd86d0e94aa446dc54e079c3ed535d39c27afa8d
Ilya Erofeev <dev.erofeev@gmail.com>
2023-05-19 21:13:18 +0600

    pages/indes: add following tabs comp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { type NextPage } from 'next';
+import { useSession } from 'next-auth/react';
+import { useState } from 'react';
 import { InfinitePostList } from '~/components/InfinitePostList';
 import { NewPostForm } from '~/components/NewPostForm';
 import { api } from '~/utils/api';
@@ -19,14 +21,52 @@ function RecentPosts() {
   );
 }
 
-const Home: NextPage = () => (
-  <>
-    <header className='sticky top-0 z-10 border-b bg-white pt-2'>
-      <h1 className='mb-2 px-4 text-lg font-bold'>Home</h1>
-    </header>
-    <NewPostForm />
-    <RecentPosts />
-  </>
-);
+function FollowingPosts() {
+  const posts = api.post.infiniteFeed.useInfiniteQuery(
+    { onlyFollowing: true },
+    { getNextPageParam: (lastPage) => lastPage.nextCursor }
+  );
+  return (
+    <InfinitePostList
+      posts={posts.data?.pages.flatMap((page) => page.posts)}
+      isError={posts.isError}
+      isLoading={posts.isLoading}
+      hasMore={posts.hasNextPage}
+      fetchNewPosts={posts.fetchNextPage}
+    />
+  );
+}
+
+const TABS = ['Recent', 'Following'];
+
+const Home: NextPage = () => {
+  const session = useSession();
+  const [selectedTab, setSelectedTab] = useState<(typeof TABS)[number]>('Recent');
+
+  return (
+    <>
+      <header className='sticky top-0 z-10 border-b bg-white pt-2'>
+        <h1 className='mb-2 px-4 text-lg font-bold'>Home</h1>
+        {session.status === 'authenticated' && (
+          <div className='flex'>
+            {TABS.map((tab) => (
+              <button
+                key={tab}
+                className={`flex-grow p-2 hover:bg-gray-200 focus-visible:bg-gray-200 ${
+                  tab === selectedTab ? 'border-b-4 border-b-blue-500 font-bold' : ''
+                }`}
+                onClick={() => setSelectedTab(tab)}
+              >
+                {tab}
+              </button>
+            ))}
+          </div>
+        )}
+      </header>
+      <NewPostForm />
+      {selectedTab === 'Recent' ? <RecentPosts /> : <FollowingPosts />}
+    </>
+  );
+};
 
 export default Home;
