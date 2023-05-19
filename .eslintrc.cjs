abc1088f63d7077fb8bf4d8f5c937bc256b0c29a
Ilya Erofeev <dev.erofeev@gmail.com>
2023-05-19 17:10:28 +0600

    update configs, packege-json

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,54 +5,67 @@ const path = require('path');
 const config = {
   overrides: [
     {
-      extends: [
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      ],
+      extends: ['plugin:@typescript-eslint/recommended-requiring-type-checking'],
       files: ['*.ts', '*.tsx'],
       parserOptions: {
         project: path.join(__dirname, 'tsconfig.json'),
       },
     },
   ],
+  env: {
+    browser: true,
+    es2021: true,
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: path.join(__dirname, 'tsconfig.json'),
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['react', '@typescript-eslint'],
   extends: [
-    'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/recommended',
     'airbnb',
+    'plugin:react/jsx-runtime',
     'prettier',
+    'plugin:import/typescript',
   ],
   rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-      },
-    ],
-    '@typescript-eslint/consistent-type-imports': [
-      'warn',
-      {
-        prefer: 'type-imports',
-        fixStyle: 'inline-type-imports',
-      },
-    ],
+    '@typescript-eslint/consistent-type-imports': ['warn'],
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    'import/extensions': 'off',
-    'react/prop-types': 'off',
-    'react/function-component-definition': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    '@typescript-eslint/no-unsafe-return': 'off',
-    '@typescript-eslint/no-unsafe-member-access': 'off',
-    '@typescript-eslint/no-unsafe-call': 'off',
     'import/prefer-default-export': 'off',
+    'import/no-default-export': 'off',
+    'react/destructuring-assignment': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
     'no-void': 'off',
-    'react/button-has-type': 'off',
+    'react/jsx-filename-extension': [0, { extensions: ['.js', '.jsx'] }],
     'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/function-component-definition': 'off',
+    'class-methods-use-this': 'off',
+    'no-unused-vars': 'warn',
+    'import/no-unresolved': 'off',
+    'no-underscore-dangle': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'react/button-has-type': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        '': 'never',
+      },
+    ],
   },
 };
 
