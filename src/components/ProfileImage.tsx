31bf28d175c7f63e177ebf2406e37b91c07ab84e
Ilya Erofeev <dev.erofeev@gmail.com>
2023-05-19 17:13:39 +0600

    ProfileImage: update, add default image

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { VscAccount } from 'react-icons/vsc';
 
 type ProfileImageProps = {
   src?: string | null;
@@ -7,10 +8,12 @@ type ProfileImageProps = {
 
 export function ProfileImage({ src, className = '' }: ProfileImageProps) {
   return (
-    <div
-      className={`relative h-12 w-12 overflow-hidden rounded-full ${className}`}
-    >
-      {src && <Image src={src} alt="Profile Image" quality={100} fill />}
+    <div className={`relative h-12 w-12 overflow-hidden rounded-full ${className}`}>
+      {src ? (
+        <Image src={src} alt='Profile Image' quality={100} fill />
+      ) : (
+        <VscAccount className='h-full w-full' />
+      )}
     </div>
   );
 }
