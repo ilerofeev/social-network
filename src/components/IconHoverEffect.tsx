e04d9152159202fbd297ada17532d4afde9af0e3
Ilya Erofeev <dev.erofeev@gmail.com>
2023-05-19 21:29:02 +0600

    IconHoverEffect: prettier

--- src/components/IconHoverEffect.tsx
+++ src/components/IconHoverEffect.tsx
@@ -10,5 +10,9 @@ export function IconHoverEffect({ children, red = false }: IconHoverEffectProps)
     ? 'outline-red-400 hover:bg-red-200 group-hover-bg-red-200 group-focus-visible:bg-red-200 focus-visible:bg-red-200'
     : 'outline-gray-400 hover:bg-gray-200 group-hover-bg-gray-200 group-focus-visible:bg-gray-200 focus-visible:bg-gray-200';
 
-  return <div className={`rounded-full p-2 transition-colors duration-200 ${colorClasses}`}>{children}</div>;
+  return (
+    <div className={`rounded-full p-2 transition-colors duration-200 ${colorClasses}`}>
+      {children}
+    </div>
+  );
 }
