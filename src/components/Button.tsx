2a3ff86156f38316113bbd1224970863cb8ba26e
Ilya Erofeev <dev.erofeev@gmail.com>
2023-05-21 15:31:13 +0600

    Button: update props and text render

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,17 +4,9 @@ type ButtonProps = {
   small?: boolean;
   gray?: boolean;
   className?: string;
-} & DetailedHTMLProps<
-  ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
->;
+} & DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
 
-export function Button({
-  small = false,
-  gray = false,
-  className = '',
-  ...props
-}: ButtonProps) {
+export function Button({ small = false, gray = false, className = '', ...props }: ButtonProps) {
   const sizeClasses = small ? 'px-2 py-1' : 'px-4 py-2 font-bold';
   const colorClasses = gray
     ? 'bg-gray-400 hover:bg-gray-300 focus-visible:bg-gray-300'
@@ -23,8 +15,6 @@ export function Button({
     <button
       className={`rounded-full text-white transition-colors duration-200 disabled:cursor-not-allowed disabled:opacity-50 ${sizeClasses} ${colorClasses} ${className}`}
       {...props}
-    >
-      Post
-    </button>
+    />
   );
 }
